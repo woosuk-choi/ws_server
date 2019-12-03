e13ad5081a1c01ac0b9ff619037f783e932465b8
woosuk <poiu1502@naver.com>
2019-12-03 21:47:10 +0900

    ajax test

--- src/main/java/ws_server/user/controller/UserController.java
+++ src/main/java/ws_server/user/controller/UserController.java
@@ -1,16 +1,35 @@
 package ws_server.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ws_server.user.service.UserService;
 
 @Controller
 public class UserController {
+	
+	@Autowired 
+	private UserService userService;
 
 	@RequestMapping("/user/openUserLogin.do")
-	public ModelAndView openUserLogin() throws Exception{
-		ModelAndView mv = new ModelAndView("user/userLogin");
+	public String openUserLogin() throws Exception{
+		return "/user/userLogin";
+	}
+	
+	@RequestMapping("/user/openUserJoin.do")
+	public String openUserJoin()throws Exception{
+		return "/user/userJoin";
+	}
+	
+	@RequestMapping("/user/selectOneUser.do")
+	@ResponseBody
+	public Boolean selectOneUser(@RequestBody String userId)throws Exception{
 		
-		return mv;
+		//boolean value = userService.selectOneUser(userId);
+		
+		return true;
 	}
 }
